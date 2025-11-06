8ccf6191b65674a945a29b830685303c7e2b3dc9
ngmnduc <minhductn0408@gmail.com>
2025-11-06 20:52:37 +0700

    add app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,22 @@ import MainLayout from "./layouts/MainLayout.jsx";
 
 function App() {
   return (
-    <>
-     <RouterProvider router={router} />
-    </>
-  )
+    <Routes>
+      {/* trang mặc định */}
+      <Route path="/" element={<Navigate to="/login" />} />
+
+      {/* login */}
+      <Route path="/login" element={<LoginPage />} />
+
+      {/* home */}
+      <Route path="/" element={<MainLayout />}>
+      <Route path="/home" element={<HomePage />} />
+      </Route>
+
+      {/* fallback 404 */}
+      <Route path="*" element={<div>404 - Page not found</div>} />
+    </Routes>
+  );
 }
 
 export default App;
