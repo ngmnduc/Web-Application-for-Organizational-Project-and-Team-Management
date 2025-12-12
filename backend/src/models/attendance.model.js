5297908b4ac7b94cd565233295a92350121a1047
Minh Hieu <minhhieu191105@gmail.com>
2025-12-12 11:03:47 +0700

    update attendance organization

--- backend/src/models/attendance.model.js
+++ backend/src/models/attendance.model.js
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 
 const attendanceSchema = new mongoose.Schema(
   {
+    organizationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+      index: true,
+    },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
